8e09a5e4da1a272762c8eae59e6f435c18edf982
BrittneyM34 <brittneym1018@yahoo.com>
2024-04-05 12:31:09 -0400

    atatus

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('953ca344690e43619876b5c1dfe1ace8').install();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
